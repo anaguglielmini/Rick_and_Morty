39d98fdaf101740cc8ccde37d30a8f2ceab18fec
ana-luizaa <ana.guglielmini@etec.sp.gov.br>
2021-09-09 20:16:13 -0300

    Criando Tela de Splash

---
+++ app/src/main/java/com/example/rickandmorty/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.rickandmorty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                SplashActivity.this,MainActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
