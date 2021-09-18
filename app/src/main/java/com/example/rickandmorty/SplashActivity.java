0ba47e04ca5317852b53d7bfb3fa8dfc80a46f72
ana-luizaa <ana.guglielmini@etec.sp.gov.br>
2021-09-18 20:35:32 -0300

    Criando Tela de Splash

--- app/src/main/java/com/example/rickandmorty/SplashActivity.java
+++ app/src/main/java/com/example/rickandmorty/SplashActivity.java
@@ -1,33 +1,65 @@
 package com.example.rickandmorty;
 
+import android.app.Service;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                mostrarMainActivity();
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
             }
-        }, 2000);
+        },5000);
+
     }
 
-    private void mostrarMainActivity() {
-        Intent intent = new Intent(
-                SplashActivity.this,MainActivity.class
-        );
-        startActivity(intent);
-        finish();
-    }
+        @Override
+        protected void onPause() {
+            super.onPause();
+            sensorManager.unregisterListener(this);
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() == Sensor.TYPE_LIGHT) {
+                if (event.values[0] == 0) {
+                    //codigo para mudar de gif
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
 }
