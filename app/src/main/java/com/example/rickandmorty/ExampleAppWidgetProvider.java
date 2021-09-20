f94abdec5d397b91d39b6065060bd845f298844c
GlaciesAurora <72575913+GlaciesAurora@users.noreply.github.com>
2021-09-20 19:45:59 -0300

    widget e portal master race

---
+++ app/src/main/java/com/example/rickandmorty/ExampleAppWidgetProvider.java
@@ -0,0 +1,24 @@
+package com.example.rickandmorty;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class ExampleAppWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, portal.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+            views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rickandmorty/portal.java
@@ -0,0 +1,60 @@
+package com.example.rickandmorty;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class portal extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor proximitySensor;
+    private SensorEventListener proximitySensorListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.portal);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if(proximitySensor == null){
+            Toast.makeText( this, "Sensor de proximidade não esta disponivel", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        proximitySensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[0] < proximitySensor.getMaximumRange()){
+                    openPagina2();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        sensorManager.registerListener(proximitySensorListener, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        sensorManager.unregisterListener(proximitySensorListener);
+    }
+    public void openPagina2(){
+        Intent intent = new Intent(this, SplashActivity.class);
+        startActivity(intent);
+    }
+
+}
